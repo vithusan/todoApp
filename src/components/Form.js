08ea2265392ce2a84eba7a9c626e087bca00c8b9
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2021-02-06 16:59:27 -0500

    added fonts and stylesheet in index, and added the form component in app.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Form = () => {
     return (
         <form>
-            <input type="text" class="todo-input" />
-            <button class="todo-button" type="submit">
-                <i class="fas fa-plus-square"></i>
+            <input type="text" className="todo-input" />
+            <button className="todo-button" type="submit">
+                <i className="fas fa-plus-square"></i>
             </button>
-            <div class="select">
-                <select name="todos" class="filter-todo">
+            <div className="select">
+                <select name="todos" className="filter-todo">
                     <option value="all">All</option>
                     <option value="completed">Completed</option>
                     <option value="uncompleted">Uncompleted</option>
@@ -17,3 +17,5 @@ const Form = () => {
         </form>
     )
 }
+
+export default Form
