8000e7daa39efd3ed9655bc92925c7b8f649fe38
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2021-02-06 17:16:33 -0500

    created todolist component and added to the app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Form from './components/Form'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h1>Todo List</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
