fcbf42bba82910ab583308af3db6a12cbabb6687
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2021-02-06 19:10:32 -0500

    added inputtext and todolist usestate and handlers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React, { useState } from 'react'
 import './App.css';
 import Form from './components/Form'
 import TodoList from './components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText} />
       <TodoList />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const Form = () => {
+const Form = ({ setInputText }) => {
+    const inputTextHandler = (e) => {
+        console.log(e.target.value)
+        setInputText(e.target.value)
+    }
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+    }
     return (
         <form>
-            <input type="text" className="todo-input" />
-            <button className="todo-button" type="submit">
+            <input onChange={inputTextHandler} type="text" className="todo-input" />
+            <button onClick={submitTodoHandler} className="todo-button" type="submit">
                 <i className="fas fa-plus-square"></i>
             </button>
             <div className="select">
