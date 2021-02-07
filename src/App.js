e509867593c6a8a48b5972692a54b2c3cc2b7153
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2021-02-07 18:29:44 -0500

    added complete and uncomplete filter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React, { useState } from 'react'
-import './App.css';
-import Form from './components/Form'
-import TodoList from './components/TodoList'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [inputText, setInputText] = useState("")
-  const [todos, setTodos] = useState([])
+  //state
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  //functions
+  const filterHanler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    filterHanler();
+  }, [todos, status]);
+
   return (
     <div className="App">
       <header>
         <h1>Todo List</h1>
       </header>
-      <Form setInputText={setInputText} />
-      <TodoList />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
-    return (
-        <div className="todo-container">
-            <ul className="todo-list"></ul>
-        </div>
-    )
-}
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            todos={todos}
+            todo={todo}
+            setTodos={setTodos}
+            key={todo.id}
+            text={todo.text}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
